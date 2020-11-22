f82b6e4d093b192e677e2366f80be68a9fb08ff9
Marcelo Vanzin <vanzin@apache.org>
2020-11-21 19:23:49 -0800

    Hook up encoding configuration UI.

--- src/cdinfo.py
+++ src/cdinfo.py
@@ -1,19 +1,23 @@
 # SPDX-License-Identifier: BSD-2-Clause
 import util
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QMessageBox
 
 
 class InfoDialog(util.compile_ui("cdinfo.ui")):
-    def __init__(self, disc):
+    def __init__(self, disc, config):
         super().__init__()
         self.setWindowModality(Qt.ApplicationModal)
 
         self.disc = disc
+        self.config = config
 
         self.btnGo.clicked.connect(self._go)
         self.btnCancel.clicked.connect(self.reject)
+        self.btnTarget.clicked.connect(self._get_target)
 
         self.leArtist.setText(disc.artist)
         self.leAlbum.setText(disc.album)
@@ -22,6 +26,10 @@ class InfoDialog(util.compile_ui("cdinfo.ui")):
 
         self.cbMultiDisc.setVisible(disc.discs > 1)
         self.cbMultiDisc.setChecked(disc.discs > 1)
+
+        self.leTarget.setText(config.target)
+        self.leEncoder.setText(config.encoder)
+        self.leTemplate.setText(config.template)
 
         self.trackNames = []
         for t in disc.tracks:
@@ -44,6 +52,16 @@ class InfoDialog(util.compile_ui("cdinfo.ui")):
     def _go(self):
         util.save_ui(self, "cdinfo")
 
+        config = {
+            "Target directory": self.leTarget,
+            "Encoder": self.leEncoder,
+            "Template": self.leTemplate,
+        }
+        for k, v in config.items():
+            if not v.text():
+                QMessageBox.critical(self, "Error", f"{k} is required.")
+                return
+
         d = self.disc
         d.artist = self.leArtist.text()
         d.album = self.leAlbum.text()
@@ -52,4 +70,12 @@ class InfoDialog(util.compile_ui("cdinfo.ui")):
         for i in range(len(d.tracks)):
             d.tracks[i].title = self.trackNames[i].text()
 
+        self.config.target = self.leTarget.text()
+        self.config.encoder = self.leEncoder.text()
+        self.config.template = self.leTemplate.text()
         self.accept()
+
+    def _get_target(self):
+        path = QFileDialog.getExistingDirectory(self)
+        if path:
+            self.leTarget.setText(path)
--- src/ripper.py
+++ src/ripper.py
@@ -1,13 +1,33 @@
 # SPDX-License-Identifier: BSD-2-Clause
+from dataclasses import dataclass
+
 import cdinfo
+import util
 from PyQt5.QtWidgets import QDialog
 
 
+@dataclass
+class Config:
+    target: str = None
+    encoder: str = None
+    template: str = None
+
+
 def rip(app, disc):
-    info = cdinfo.InfoDialog(disc)
+    config = Config(
+        target=util.SETTINGS.value("fripper/target"),
+        encoder=util.SETTINGS.value("fripper/encoder"),
+        template=util.SETTINGS.value("fripper/template"),
+    )
+
+    info = cdinfo.InfoDialog(disc, config)
     if info.exec_() == QDialog.Rejected:
         app.quit()
         return
+
+    util.SETTINGS.setValue("fripper/target", config.target)
+    util.SETTINGS.setValue("fripper/encoder", config.encoder)
+    util.SETTINGS.setValue("fripper/template", config.template)
 
     print("TODO: rip it")
     app.quit()
