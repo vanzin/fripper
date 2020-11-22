b742c8684790b58ae7e1ce5dd2b5d0389318e971
Marcelo Vanzin <vanzin@apache.org>
2020-11-21 19:43:38 -0800

    Initial ripping progress UI.

--- src/ripper.py
+++ src/ripper.py
@@ -13,6 +13,29 @@ class Config:
     template: str = None
 
 
+class RipperDialog(util.compile_ui("ripper.ui")):
+    def __init__(self, disc, config):
+        super().__init__()
+
+        count = len(disc.tracks)
+        self.pbEncoder.setMinimum(0)
+        self.pbEncoder.setMaximum(count)
+        self.lEncoderCompleted.setText(self._completed(0, count))
+
+        self.pbRipper.setMinimum(0)
+        self.pbRipper.setMaximum(len(disc.tracks))
+        self.lRipperCompleted.setText(self._completed(0, count))
+
+        self.btnCancel.clicked.connect(self._cancel)
+
+    def _completed(self, done, target):
+        return f"{done}/{target}"
+
+    def _cancel(self):
+        # TODO
+        self.reject()
+
+
 def rip(app, disc):
     config = Config(
         target=util.SETTINGS.value("fripper/target"),
@@ -28,6 +51,9 @@ def rip(app, disc):
     util.SETTINGS.setValue("fripper/target", config.target)
     util.SETTINGS.setValue("fripper/encoder", config.encoder)
     util.SETTINGS.setValue("fripper/template", config.template)
+
+    ripper = RipperDialog(disc, config)
+    ripper.exec_()
 
     print("TODO: rip it")
     app.quit()
